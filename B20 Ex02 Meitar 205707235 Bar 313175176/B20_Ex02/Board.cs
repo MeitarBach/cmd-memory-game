7b6e6393e7a17a55e0218c718b86c3388a473d60
BB <SD>
2020-05-22 18:44:05 +0300

    try createRandomizeBoard

--- B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/Board.cs
+++ B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/Board.cs
@@ -12,7 +12,7 @@
             m_Width = i_Width;
             m_BoardCells = new GameCell[m_Height, m_Width];
 
-            //createRandomizeBoard
+            createRandomizeBoard();
         }
 
         internal int Height
@@ -35,37 +35,57 @@
         {
             get
             {
-                return m_BoardCells;
+                return m_BoardCells(i_Height, i_Width);
             }
         }
 
 
         private void createRandomizeBoard()
         {
-            bool isEven = false;
+            bool counterOfChars = 0;
+            char temporaryChar;
             List<char> listOfChar = new List<Char>();
 
             for(int i = 0; i < m_Height; i++)
             {
                 for(int j = 0; j < m_Width; j++)
                 {
-                    if(!isEven)
+                    if(counterOfChars % 2 == 0)
                     {
-                        m_BoardCells[i, j].letter = (char)charRandom.Next('a', 'Z');
+                        temporaryChar = (char) ('A' + counter);
+                        m_BoardCells[i, j].letter = temporaryChar;
+                        counter++;
                     }
                     else
                     {
-                        
+                        m_BoardCells[i, j].letter = temporaryChar;
                     }
-
                 }
             }
-            // Random charRandom = new Random(); 
-            // m_BoardCells[i, j].letter = (char)charRandom.Next('A', 'Z');
 
+            shuffle();
+        }
 
-            
+        private void shuffle()
+        {
+            numberOfCells = m_Height * m_Width;
+            Random random = new Random();
 
+            for(int i = 0; i < numberOfCells - 1; i++)
+            {
+                // Pick a random cell between i and the end of the array.
+                int j = rand.Next(i, num_cells);
+                int i0 = i / m_Width;
+                int i1 = i % m_Width;
+
+                int j = random.Next(i + 1);
+                int j0 = j / m_Width;
+                int j1 = j % m_Width;
+
+                GameCell temp = m_BoardCells[i0, i1];
+                m_BoardCells[i0, i1] = m_BoardCells[j0, j1];
+                m_BoardCells[j0, j1] = temp;
+            }
         }
     }
 }
