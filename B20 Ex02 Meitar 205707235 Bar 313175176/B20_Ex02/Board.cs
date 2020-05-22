617290f8d525a55febc5c66327fc24f7ba71f88e
BB <SD>
2020-05-22 15:58:58 +0300

    GameCell almost finish
    GameCell almost finish
    Board need to create  createRandomizeBoard

--- B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/Board.cs
+++ B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/Board.cs
@@ -4,11 +4,15 @@
     {
         private int m_Height;
         private int m_Width;
+        private GameCell[,] m_BoardCells;
 
         internal Board(int i_Height, int i_Width)
         {
             m_Height = i_Height;
             m_Width = i_Width;
+            m_BoardCells = new GameCell[m_Height, m_Width];
+
+            //createRandomizeBoard
         }
 
         internal int Height
@@ -25,6 +29,13 @@
             {
                 return m_Width;
             }
+        }
+
+        private void createRandomizeBoard()
+        {
+            // Random charRandom = new Random(); 
+            // m_BoardCells[i, j].letter = (char)charRandom.Next('A', 'Z');
+
         }
     }
 }
---
+++ B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/GameCell.cs
@@ -0,0 +1,35 @@
+﻿namespace B20_Ex02
+{
+    public class GameCell
+    {
+        private readonly char r_Letter;
+        private bool m_CellIsShow;
+
+        internal Player(char i_Letter)
+        {
+            r_Letter = i_Letter;
+            m_CellIsShow = false;
+        }
+
+        internal char Letter
+        {
+            get
+            {
+                return r_Letter;
+            }
+        }
+
+        internal bool CellIsShow
+        {
+            get
+            {
+                return m_CellIsShow;
+            }
+
+            set
+            {
+                m_CellIsShow = value;
+            }
+        }
+    }
+}
