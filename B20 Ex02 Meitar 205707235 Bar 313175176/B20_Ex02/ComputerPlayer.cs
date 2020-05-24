6fa93703d8e4a829dbb5ac95f2ed4a27bdd33a24
MeitarBach <meitar.bach666@gmail.com>
2020-05-25 00:17:40 +0300

    Computer Player Updates

--- B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/ComputerPlayer.cs
+++ B20 Ex02 Meitar 205707235 Bar 313175176/B20_Ex02/ComputerPlayer.cs
@@ -8,15 +8,15 @@ namespace B20_Ex02
     {
         private ushort m_Score;
         private readonly eGameLevel r_GameLevel;
-        private List<GameCell> m_RevealedCells;
+        private Dictionary<char, GameCell> m_RevealedCells;
         private Stack<GameCell> m_RevealedCouples;
-        private char m_lastGameCellSelected = '\0';
+        private char m_lastGameCellSelected = '0'; // A char which isn't on the board
 
-        internal ComputerPlayer(ushort i_Score, eGameLevel i_GameLevel)
+        internal ComputerPlayer(eGameLevel i_GameLevel)
         {
-            m_Score = i_Score;
+            m_Score = 0;
             r_GameLevel = i_GameLevel;
-            m_RevealedCells = new List<GameCell>();
+            m_RevealedCells = new Dictionary<char, GameCell>();
             m_RevealedCouples = new Stack<GameCell>();
         }
 
@@ -44,42 +44,52 @@ namespace B20_Ex02
         {
             GameCell gameCellSelect;
 
-            if(m_lastGameCellSelected == '\0') // First turn
+            if(m_lastGameCellSelected == '0') // First half of turn
             {
-                if(coupleRevealedinStack()) // If there is a revealed couple
+                if(unRevealedCoupleInStack()) // There is an unrevealed couple
                 {
-                    gameCellSelect = m_RevealedCouples.Pop();
+                    gameCellSelect = m_RevealedCouples.Pop(); // Pop first cell of the couple
                 }
                 else
                 {
-                    gameCellSelect = randomizeCell(i_Board);
+                    gameCellSelect = chooseRandomCell(i_Board);
+                    AnalyzeMove(i_Board, gameCellSelect);
                 }
 
                 m_lastGameCellSelected = gameCellSelect.Letter;
             }
-            else // second turn
+            else // Second half turn
             {
                 if((gameCellSelect = findMatch()) == null) // If there is a match
                 {
-                    gameCellSelect = randomizeCell(i_Board);
+                    gameCellSelect = chooseRandomCell(i_Board);
+                    AnalyzeMove(i_Board, gameCellSelect);
                 }
 
-                m_lastGameCellSelected = '\0';
+                m_lastGameCellSelected = '0';
             }
 
-            AnalyzeMove(i_Board, gameCellSelect);
+            
 
             return gameCellSelect;
         }
 
         internal void AnalyzeMove(Board i_Board, GameCell i_GameCell)
         {
-            GameCell gameCellMatch = null;
+            GameCell gameCellMatch;
 
-            if ((gameCellMatch = gameCellContainsAndMatch(i_GameCell.Letter)) != null)
+            if (m_RevealedCells.TryGetValue(i_GameCell.Letter, out gameCellMatch)) // check if i
             {
-                m_RevealedCouples.Push(i_GameCell);
-                m_RevealedCouples.Push(gameCellMatch);
+                if(m_lastGameCellSelected == gameCellMatch.Letter)
+                {
+                    m_RevealedCouples.Push(gameCellMatch);
+                }
+                else
+                {
+                    m_RevealedCouples.Push(i_GameCell);
+                    m_RevealedCouples.Push(gameCellMatch);
+                }
+                
                 i_Board.UnRevealedCells.Remove(i_GameCell);
             }
             else
@@ -91,83 +101,65 @@ namespace B20_Ex02
         private void addByLevelGame(Board i_Board, GameCell i_GameCell)
         {
             Random random = new Random();
-            int randomizeToAdd = random.Next(10);
+            int rememberProbability = random.Next(10);
+            bool rememberCell = false;
 
             switch (r_GameLevel)
             {
                 case eGameLevel.Easy:
-                    if(randomizeToAdd <= 4) // 50% (Half) of the times algorithm will add
-                    {
-                        m_RevealedCells.Add(i_GameCell);
-                        i_Board.UnRevealedCells.Remove(i_GameCell);
-                    }
-
+                    rememberCell = rememberProbability <= 4; // 50% (Half) of the times algorithm will add
                     break;
                 case eGameLevel.Medium:
-                    if (randomizeToAdd <= 7) // 80% the times algorithm will add
-                    {
-                        m_RevealedCells.Add(i_GameCell);
-                        i_Board.UnRevealedCells.Remove(i_GameCell);
-                    }
-
+                    rememberCell = rememberProbability <= 7; // 80% of the times algorithm will add
                     break;
-                case eGameLevel.Hard: // 100% the times algorithm will add
-                    m_RevealedCells.Add(i_GameCell);
-                    i_Board.UnRevealedCells.Remove(i_GameCell);
+                case eGameLevel.Hard: // 100% of the times algorithm will add
+                    rememberCell = true;
                     break;
             }
 
-        }
-
-        private GameCell gameCellContainsAndMatch(char i_CharToCheck)
-        {
-            GameCell gameCellMatch = null;
-
-            foreach (GameCell gameCell in m_RevealedCells)
+            if(rememberCell)
             {
-                if (i_CharToCheck == gameCell.Letter)
-                {
-                    gameCellMatch = gameCell;
-                }
+                m_RevealedCells.Add(i_GameCell.Letter, i_GameCell);
+                i_Board.UnRevealedCells.Remove(i_GameCell);
             }
-
-            return gameCellMatch;
         }
 
         private GameCell findMatch()
         {
             GameCell gameCellMatch = null;
 
-            if(m_lastGameCellSelected == m_RevealedCouples.Peek().Letter)
+            if(m_lastGameCellSelected == m_RevealedCouples.Peek().Letter) 
             {
                 gameCellMatch = m_RevealedCouples.Pop();
             }
             else
             {
-                gameCellMatch = gameCellContainsAndMatch(m_lastGameCellSelected);
+                m_RevealedCells.TryGetValue(m_lastGameCellSelected, out gameCellMatch);
             }
 
             return gameCellMatch;
         }
 
-        private GameCell randomizeCell(Board i_Board)
+        private GameCell chooseRandomCell(Board i_Board)
         {
             Random random = new Random();
             int gameCellIndex = random.Next(i_Board.UnRevealedCells.Count);
             return i_Board.UnRevealedCells[gameCellIndex];
         }
 
-        private bool coupleRevealedinStack()
+        private bool unRevealedCoupleInStack()
         {
             bool existsCouple = false;
-
-            if (m_RevealedCouples.Peek().IsRevealed == false)
-            { 
-                existsCouple = true; 
-            }
-            else // Clear 2 GameCell in the stack if there are cells that Revealed
-            { 
-                m_RevealedCouples.Pop(); 
+            while(m_RevealedCouples.Count != 0) // stack isn't empty
+            {
+                if (!m_RevealedCouples.Peek().IsRevealed)
+                {
+                    existsCouple = true;
+                    break;
+                }
+                
+                //// Remove Revealed couple from stack
+                m_RevealedCouples.Pop();
                 m_RevealedCouples.Pop();
             }
 
